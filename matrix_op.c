a806fed101dc31a8abb4163fc2f992bdfe52f7d3
ycling_09.10 <yu20060910@gmail.com>
2025-12-16 20:37:02 +0800

    Update matrix_op.c

--- matrix_op.c
+++ matrix_op.c
@@ -92,3 +92,40 @@ Matrix matrix_transpose(Matrix A) {
     }
     return result;
 }
+// 實作 7: 進階運算 - 行列式 (Det(A))
+double matrix_determinant(Matrix A) {
+    double det;
+    // 使用薩魯斯法則 (Sarrus' Rule)
+    // det = a(ei - fh) - b(di - fg) + c(dh - eg)
+    
+    det = A.data[0][0] * (A.data[1][1] * A.data[2][2] - A.data[1][2] * A.data[2][1]) -
+          A.data[0][1] * (A.data[1][0] * A.data[2][2] - A.data[1][2] * A.data[2][0]) +
+          A.data[0][2] * (A.data[1][0] * A.data[2][1] - A.data[1][1] * A.data[2][0]);
+          
+    return det;
+}
+// 實作 8: 進階運算 - 伴隨矩陣 (Adj(A))
+Matrix matrix_adjoint(Matrix A) {
+    Matrix adj;
+    
+    // adj[i][j] 是 A 的元素 A[j][i] 的代數餘子式
+    
+    // 代數餘子式 C[i][j] = (-1)^(i+j) * M[i][j]
+    
+    // 計算第一行元素的代數餘子式轉置後，填入 adj 的第一列 (adj[i][0] => A[0][i])
+    adj.data[0][0] = det_2x2(A.data[1][1], A.data[1][2], A.data[2][1], A.data[2][2]); // A[0][0] 餘子式
+    adj.data[1][0] = -det_2x2(A.data[1][0], A.data[1][2], A.data[2][0], A.data[2][2]);// A[0][1] 餘子式 (變號)
+    adj.data[2][0] = det_2x2(A.data[1][0], A.data[1][1], A.data[2][0], A.data[2][1]); // A[0][2] 餘子式
+    
+    // 計算第二行元素的代數餘子式轉置後，填入 adj 的第二列
+    adj.data[0][1] = -det_2x2(A.data[0][1], A.data[0][2], A.data[2][1], A.data[2][2]);// A[1][0] 餘子式 (變號)
+    adj.data[1][1] = det_2x2(A.data[0][0], A.data[0][2], A.data[2][0], A.data[2][2]); // A[1][1] 餘子式
+    adj.data[2][1] = -det_2x2(A.data[0][0], A.data[0][1], A.data[2][0], A.data[2][1]);// A[1][2] 餘子式 (變號)
+
+    // 計算第三行元素的代數餘子式轉置後，填入 adj 的第三列
+    adj.data[0][2] = det_2x2(A.data[0][1], A.data[0][2], A.data[1][1], A.data[1][2]); // A[2][0] 餘子式
+    adj.data[1][2] = -det_2x2(A.data[0][0], A.data[0][2], A.data[1][0], A.data[1][2]);// A[2][1] 餘子式 (變號)
+    adj.data[2][2] = det_2x2(A.data[0][0], A.data[0][1], A.data[1][0], A.data[1][1]); // A[2][2] 餘子式
+    
+    return adj;
+}
